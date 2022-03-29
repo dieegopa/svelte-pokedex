0bf45e835b43c6f5c32974472835d618c1b6a7db
dieegopa <dpadilla>
2022-03-29 17:47:05 +0200

    Update About page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,16 +1,30 @@
 <script>
 	import imagen from "../assets/untitled1.png";
+	import Icon from "svelte-icons-pack/Icon.svelte";
+	import AiFillGithub from "svelte-icons-pack/ai/AiFillGithub";
 </script>
 
 <svelte:head>
-	<title>Acerca</title>
+	<title>About</title>
 </svelte:head>
 
 <div class="space-y-5">
-    <p class="text-center">Created by</p>
-	<a class="text-center font-bold text-2xl underline block hover:text-orange-500 duration-300 transition-all" href="https://diegopa.com" target="blank">Diego Padilla</a>
+	<p class="text-center">Created by</p>
+	<a
+		class="text-center font-bold text-2xl underline block hover:text-orange-500 duration-300 transition-all"
+		href="https://diegopa.com"
+		target="blank">Diego Padilla</a
+	>
 	<div class="flex justify-center">
 		<img src={imagen} alt="Diego Padilla" class="h-96" />
+	</div>
+	<div class="flex justify-center space-x-1 items-center text-orange-500 font-bold text-xl ">
+		<Icon src={AiFillGithub} color="#ea580c" />
+		<a
+			href="https://github.com/dieegopa/svelte-pokedex"
+			class="block text-center hover:text-orange-800 transition-all duration-300 ease-in-out"
+			target="blank">Github Repository</a
+		>
 	</div>
 </div>
 
